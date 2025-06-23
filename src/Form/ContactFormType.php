70a4f89de0cffd6ad063d14ae24f3aa75a9946e4
rafiksellah <rafik@theagency-dz.com>
2025-06-23 14:43:15 +0100

    Ajout de la prise en charge de Google Recaptcha dans le formulaire de contact, y compris la configuration des clés et l'intégration du service.

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -3,19 +3,20 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use VictorPrdh\RecaptchaBundle\Form\ReCaptchaType;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ContactFormType extends AbstractType
 {
@@ -115,7 +116,7 @@ class ContactFormType extends AbstractType
                     'rows' => 5
                 ],
             ])
-        ;
+            ->add("recaptcha", ReCaptchaType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
